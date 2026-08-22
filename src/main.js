31f49bcf73fa22a132ca433299aba0dbd3424a0c
Trung Nguyen <nngu0066@student.monash.edu>
2026-08-22 17:40:17 +1000

    Task 4.1 and 4.2 Commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,26 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-//import './style.css'
-// import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import ColumnGroup from 'primevue/columngroup' // optional
+import Row from 'primevue/row' // optional
+
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.component('DataTable', DataTable)
+app.component('Column', Column)
+app.component('ColumnGroup', ColumnGroup)
+app.component('Row', Row)
+
+app.mount('#app')
